68e691001d440bd4c0c4be8ec71c2d869709892e
Joel Höner <athre0z@zyantific.com>
2017-09-08 03:27:42 +0200

    Cleanup in `build.rs`, updated `zydis-c`

--- build.rs
+++ build.rs
@@ -9,20 +9,29 @@ use gcc::Build;
 
 const ZYDIS_INCLUDE_PATH: &'static str = "zydis-c/include";
 const ZYDIS_SRC_PATH: &'static str = "zydis-c/src";
+const ZYDIS_DEFINES: &'static [&'static str] = &[
+    "ZYDIS_ENABLE_FEATURE_EVEX",
+    "ZYDIS_ENABLE_FEATURE_MVEX",
+    "ZYDIS_ENABLE_FEATURE_FLAGS",
+    "ZYDIS_ENABLE_FEATURE_DECODER",
+    "ZYDIS_ENABLE_FEATURE_ENCODER",
+];
 
 fn build_library() {
-    Build::new()
+    let mut build = Build::new();
+
+    for cur_define in ZYDIS_DEFINES {
+        build.define(cur_define, None);
+    }
+
+    build
         .include(ZYDIS_INCLUDE_PATH)
         .include(ZYDIS_SRC_PATH)
         .include("src")
-        .define("ZYDIS_ENABLE_FEATURE_EVEX", None)
-        .define("ZYDIS_ENABLE_FEATURE_MVEX", None)
-        .define("ZYDIS_ENABLE_FEATURE_FLAGS", None)
-        .define("ZYDIS_ENABLE_FEATURE_DECODER", None)
-        .define("ZYDIS_ENABLE_FEATURE_ENCODER", None)
         .files(vec![
             format!("{}/Decoder.c", ZYDIS_SRC_PATH),
             format!("{}/DecoderData.c", ZYDIS_SRC_PATH),
+            format!("{}/MetaInfo.c", ZYDIS_SRC_PATH),
             format!("{}/Encoder.c", ZYDIS_SRC_PATH),
             format!("{}/EncoderData.c", ZYDIS_SRC_PATH),
             format!("{}/Formatter.c", ZYDIS_SRC_PATH),
@@ -36,22 +45,23 @@ fn build_library() {
 }
 
 fn build_bindings(out_path: PathBuf) {
-    let bindings = Builder::default()
-        .unstable_rust(true)
+    let mut bindings = Builder::default()
+        .unstable_rust(false)
         .header(format!("{}/Zydis/Zydis.h", ZYDIS_INCLUDE_PATH))
         //.header(format!("{}/Zydis/Encoder.h", ZYDIS_INCLUDE_PATH))
         .clang_arg(format!("-I{}", ZYDIS_INCLUDE_PATH))
         .clang_arg(format!("-I{}", ZYDIS_SRC_PATH))
         .clang_arg("-Isrc")
-        .clang_arg("-DZYDIS_ENABLE_FEATURE_EVEX")
-        .clang_arg("-DZYDIS_ENABLE_FEATURE_MVEX")
-        .clang_arg("-DZYDIS_ENABLE_FEATURE_FLAGS")
-        .clang_arg("-DZYDIS_ENABLE_FEATURE_DECODER")
-        .clang_arg("-DZYDIS_ENABLE_FEATURE_ENCODER")
         .emit_builtins()
         .constified_enum("Zydis.*")
         .layout_tests(true)
-        .prepend_enum_name(false)
+        .prepend_enum_name(false);
+
+    for cur_define in ZYDIS_DEFINES {
+        bindings = bindings.clang_arg(format!("-D{}", cur_define));
+    }
+
+    let bindings = bindings
         .generate()
         .expect("Could not generate bindings to zydis");
 
