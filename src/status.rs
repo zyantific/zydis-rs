8733305d33ad02d4116f8a440d7b0b35ba20253d
th0rex <c0untlizzi@gmail.com>
2018-06-30 11:25:50 +0200

    Export the `check` macro

--- src/status.rs
+++ src/status.rs
@@ -5,6 +5,7 @@ use gen::ZydisStatus;
 // A Zydis result, holding either a result or a failure code.
 pub type ZydisResult<T> = Result<T, ZydisStatus>;
 
+#[macro_export]
 macro_rules! check {
     ($expression:expr, $ok:expr) => {
         match $expression as _ {
@@ -26,11 +27,10 @@ macro_rules! check_option {
 }
 
 macro_rules! check_string {
-    ($expression:expr) => { {
-            match $expression {
-                x if x.is_null() => None,
-                x => Some(CStr::from_ptr(x).to_str().unwrap())
-            }
+    ($expression:expr) => {{
+        match $expression {
+            x if x.is_null() => None,
+            x => Some(CStr::from_ptr(x).to_str().unwrap()),
         }
-    };
+    }};
 }
