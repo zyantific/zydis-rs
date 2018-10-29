4fcd2d3e186b08548c8d9a054b64d0d2b0fa41ea
Willi Ballenthin <wilbal1087@gmail.com>
2018-10-29 13:15:29 -0600

    build.rs: better detect build profile using cmake-rs method
    see: #13.

--- build.rs
+++ build.rs
@@ -2,6 +2,82 @@ extern crate cmake;
 
 use std::env;
 
+/// Determine the CMAKE_BUILD_TYPE profile that will be used given the current
+/// build options.
+/// 
+/// Note: this implementation comes from the `cmake-rs` crate and should stay
+/// in sync.
+///
+/// Via that project: the profile is automatically inferred from Rust's 
+/// compilation profile as follows:
+///
+/// * if `opt-level=0` then `CMAKE_BUILD_TYPE=Debug`,
+/// * if `opt-level={1,2,3}` and:
+///   * `debug=false` then `CMAKE_BUILD_TYPE=Release`
+///   * otherwise `CMAKE_BUILD_TYPE=RelWithDebInfo`
+/// * if `opt-level={s,z}` then `CMAKE_BUILD_TYPE=MinSizeRel`
+fn get_cmake_profile() -> &'static str {
+    // via: https://github.com/alexcrichton/cmake-rs/blob/master/src/lib.rs#L427
+
+    // Determine Rust's profile, optimization level, and debug info:
+    #[derive(PartialEq)]
+    enum RustProfile {
+        Debug,
+        Release,
+    }
+    #[derive(PartialEq, Debug)]
+    enum OptLevel {
+        Debug,
+        Release,
+        Size,
+    }
+
+    let rust_profile = match &env::var("PROFILE").unwrap()[..] {
+        "debug" => RustProfile::Debug,
+        "release" | "bench" => RustProfile::Release,
+        unknown => {
+            eprintln!(
+                "Warning: unknown Rust profile={}; defaulting to a release build.",
+                unknown
+            );
+            RustProfile::Release
+        }
+    };
+
+    let opt_level = match &env::var("OPT_LEVEL").unwrap()[..] {
+        "0" => OptLevel::Debug,
+        "1" | "2" | "3" => OptLevel::Release,
+        "s" | "z" => OptLevel::Size,
+        unknown => {
+            let default_opt_level = match rust_profile {
+                RustProfile::Debug => OptLevel::Debug,
+                RustProfile::Release => OptLevel::Release,
+            };
+            eprintln!(
+                "Warning: unknown opt-level={}; defaulting to a {:?} build.",
+                unknown, default_opt_level
+            );
+            default_opt_level
+        }
+    };
+
+    let debug_info: bool = match &env::var("DEBUG").unwrap()[..] {
+        "false" => false,
+        "true" => true,
+        unknown => {
+            eprintln!("Warning: unknown debug={}; defaulting to `true`.", unknown);
+            true
+        }
+    };
+
+    match (opt_level, debug_info) {
+        (OptLevel::Debug, _) => "Debug",
+        (OptLevel::Release, false) => "Release",
+        (OptLevel::Release, true) => "RelWithDebInfo",
+        (OptLevel::Size, _) => "MinSizeRel",
+    }
+}
+
 fn build_library() {
     let mut config = cmake::Config::new("zydis-c");
 
@@ -12,16 +88,9 @@ fn build_library() {
     let dst = config.build();
 
     let target = env::var("TARGET").unwrap_or("(unknown)".to_string());
-    let profile = env::var("PROFILE").unwrap_or("(unknown)".to_string());
     let is_msvc = target.ends_with("windows-msvc");
 
-    let relative_build_dir = if is_msvc {
-        // ref: https://docs.rs/cmake/0.1.24/src/cmake/lib.rs.html#323
-        match &profile[..] {
-            "bench" | "release" => "Release",
-            _ => "Debug",
-        }
-    } else { "" };
+    let relative_build_dir = if is_msvc { get_cmake_profile() } else { "" };
 
     println!(
         "cargo:rustc-link-search=native={}/build/{}",
