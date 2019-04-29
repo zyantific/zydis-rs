7f2feaa74f81cc24636adec78c4d42f259ca5d76
th0rex <c0untlizzi@gmail.com>
2019-04-29 20:04:37 +0200

    Only allow overflowing_literals for wasm

--- src/status.rs
+++ src/status.rs
@@ -71,7 +71,7 @@ pub enum Status {
     // Don't use this, it is used so that you always have a `_` in all match patterns, because
     // otherwise we could hit undefined behaviour.
     #[doc(hidden)]
-    #[allow(overflowing_literals)]
+    #[cfg_attr(feature = "wasm", allow(overflowing_literals))]
     __NoExhaustiveMatching__ = 0xFFFFFFFF,
 }
 
